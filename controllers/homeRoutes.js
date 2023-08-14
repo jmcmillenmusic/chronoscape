21dc8823c18ba311aa757ddd57fd8cfbaa0cb289
Rockojoe2 <lee.d.joey@gmail.com>
2023-08-13 20:39:51 -0500

    Got homepage to show something

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,28 +1,30 @@
 const router = require('express').Router();
 const Card = require('../models/Card'); 
 const ContinueCard = require('../models/ContinueCard');
+const Question = require('../models/Question')
+const Answer = require('../models/Answer')
 
-router.get('/', async (req, res) => {
-    try {
-        //Below loads all cards and shows the description and answer choices
+// router.get('/', async (req, res) => {
+//     try {
+//         //Below loads all cards and shows the description and answer choices
 
-        const continueCardData = await ContinueCard.findByPk(1, {attributes: ['title', 'description']});
-        const cardData = await Card.findAll({raw: true, attributes: ['description', 'answerChoice1', 'answerChoice2', 'answerChoice3']})
+//         const continueCardData = await ContinueCard.findByPk(1, {attributes: ['title', 'description']});
+//         const cardData = await Card.findAll({raw: true, attributes: ['description', 'answerChoice1', 'answerChoice2', 'answerChoice3']})
 
-        //Show this for now
-        // res.status(200).json(cardData);
-        const continueCard = continueCardData.get({ plain: true }); 
-        // const card = cardData.get({ plain: true }); 
-        // console.log("LOOK AT THIS" + cardData.title + cardData.description);
-        console.log(cardData);
-        res.render('homepage', { continueCard, cardData }); // Pass the continueCard data to the view
+//         //Show this for now
+//         // res.status(200).json(cardData);
+//         const continueCard = continueCardData.get({ plain: true }); 
+//         // const card = cardData.get({ plain: true }); 
+//         // console.log("LOOK AT THIS" + cardData.title + cardData.description);
+//         console.log(cardData);
+//         res.render('homepage', { continueCard, cardData }); // Pass the continueCard data to the view
         
-    }
-    catch(err)
-    {
-        res.status(500).json(err);
-    }
-});
+//     }
+//     catch(err)
+//     {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.get('/cards/:id', async (req, res) => {
     try {
@@ -45,6 +47,32 @@ router.get('continueCards/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const questionData = await Question.findAll({
+        include: [
+          {
+            model: Answer,
+            attributes: ['answerChoice'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const questions = questionData.map((question) => question.get({ plain: true }));
+    //   console.log(questions);
+      
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        questions
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,22 @@
 const sequelize = require('../config/connection');
-const { Question, Answer, Location} = require('../models');
+const { Question, Answer, Location, Card, ContinueCard} = require('../models');
+const cardData = require('./cardData.json');
+const continueCardData = require('./continueCardData.json');
 const questions = require('./questionData.json');
 const answers = require('./answersData.json');
 const locations = require('./locationData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
+
+    const cards = await Card.bulkCreate(cardData, {
+      returning: true,
+    });
+
+    const continueCards = await ContinueCard.bulkCreate(continueCardData, {
+      returning: true,
+    });
+
   
     const question = await Question.bulkCreate(questions, {
         returning: true,
